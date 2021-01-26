8f491f22b7539aa7129e4d6abab35dfeeb33faf6
thayeylolu <hamdallahhassanah@gmail.com>
2021-01-26 23:31:44 +0100

    The naive Bayes Algo

---
+++ ml algorithms/naive bayes/naives.py
@@ -0,0 +1,12 @@
+def conditional_probability ():
+    """
+    conditional probability of an Event A given that an event B occurs
+    """
+    pass 
+
+def naives_bayes():
+    """
+    This algorithm for Naives Bayes
+    """
+
+    pass
