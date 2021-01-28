0eba860550524053d21be80530becb5030b377fe
thayeylolu <hamdallahhassanah@gmail.com>
2021-01-28 22:24:21 +0100

    Seoerating all classes independently

--- ml algorithms/naive bayes/naives.py
+++ ml algorithms/naive bayes/naives.py
@@ -17,3 +17,16 @@ def naives_bayes():
     """
 
     pass
+
+#https://github.com/thayeylolu/Machine-Learning-Algorithms
+
+# Split the dataset by class values, returns a dictionary
+def separate_by_class(dataset):
+	separated = dict()
+	for i in range(len(dataset)):
+		vector = dataset[i]
+		class_value = vector[-1]
+		if (class_value not in separated):
+			separated[class_value] = list()
+		separated[class_value].append(vector)
+	return separated
